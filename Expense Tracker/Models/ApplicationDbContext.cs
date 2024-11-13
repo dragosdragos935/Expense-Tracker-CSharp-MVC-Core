925feea6ffe2bd8d4cb312150538a8419c25cf3b
Matei Dragos <53956660+dragosdragos935@users.noreply.github.com>
2024-11-14 00:05:55 +0200

    Expense Tracker v1.0 C# MVC Core

---
+++ Expense Tracker/Models/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Expense_Tracker.Models;
+using Microsoft.EntityFrameworkCore;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    { }
+
+    public DbSet<Transaction> Transactions { get; set; }
+    public DbSet<Category> Categories { get; set; }
+}
